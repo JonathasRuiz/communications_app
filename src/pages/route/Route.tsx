b06de6baa4bf0ba776ecc263a53c1d8df0d2e17a
Jonathas Ruiz <jonathasruiz1@gmail.com>
2026-05-07 14:31:37 -0600

    WC-XX [FE] swap the env variable for GOOGLE api key

--- src/pages/route/Route.tsx
+++ src/pages/route/Route.tsx
@@ -32,9 +32,11 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import PendingIcon from '@mui/icons-material/Pending';
 import axios from 'axios';
 import { getTrackerHost, getTrackerServerGoHost, MQTT_WS_URL, getTrackerAnalyticsHost } from '../../utils/server';
+import { env } from '../../utils/env';
 import { useSimulationMqtt, SimulationNotification } from '../../hooks/useSimulationMqtt';
 
-const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? '';
+// Use runtime environment configuration (works with both build-time and runtime env vars)
+const API_KEY = env.REACT_APP_GOOGLE_MAPS_API_KEY;
 const SIMULATION_REQUEST_URL = `${getTrackerHost()}/simulations/request`;
 const NEXT_GEN_SIMULATION_REQUEST_URL = `${getTrackerServerGoHost()}/simulations/request`;
 
